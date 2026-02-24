4dce3b5e21689c03d332e9afa37184899ddec43e
Santy23456 <santimoneb24@gmail.com>
2026-02-23 21:55:16 -0500

    VETERINARIA SANTI

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+        DecimalFormat of = new DecimalFormat("0.00");
+        Scanner teclado = new Scanner(System.in);
+    }
